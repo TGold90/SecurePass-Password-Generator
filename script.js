daa511d51c37b58f38cc3d928dac847c5161398d
TGOLD <thomaswgold90@gmail.com>
2023-11-28 18:35:10 -0700

    cleanup

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var generateBtn = document.querySelector("#generate");
 
 // these are all of the possible character options defined into separate variables
 var specialChar = ['@', '$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '"', "'", '_'];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
 var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 function criteriaOptions() {
   var pwLength = parseInt(prompt("Please select the character length of your password: between 8 and 128 characters."));
   if (pwLength < 8 || pwLength > 128) {
-    alert ("Password length must be between 8 and 128 characters.");
+    alert("Password length must be between 8 and 128 characters.");
     return null;
   }
   //confirm essentially gives a boolean option: true/false
@@ -20,15 +20,11 @@ function criteriaOptions() {
   var lowerCaseConfirm = confirm("Would you like to include lower case letters?")
   var upperCaseConfirm = confirm("Would you like to include upper case letters?")
 
-  if (
-      specialCharConfirm === false &&
-      numbersConfirm === false &&
-      lowerCaseConfirm === false &&
-      upperCaseConfirm === false) {
-        alert("You must select at least 1 criteria");
-        return null;
-      }
-    
+  if (!specialCharConfirm && !numbersConfirm && !lowerCaseConfirm && !upperCaseConfirm) {
+    alert("You must select at least 1 criteria");
+    return null;
+  }
+
   var criteriaSelection = {
     pwLength: pwLength,
     specialCharConfirm: specialCharConfirm,
@@ -37,7 +33,7 @@ function criteriaOptions() {
     upperCaseConfirm: upperCaseConfirm,
   };
   return criteriaSelection;
-} 
+}
 
 // Write password to the #password input
 //invokes generate password
@@ -68,32 +64,20 @@ function generatePassword() {
   //concatenate all choices we made
   if (options.specialCharConfirm) {
     allCharacters = allCharacters.concat(specialChar);
-    //will push a random amount of special characters using math.random
-    randomChars.push(randomNum(specialChar));
-    // have to add pw.Length-- so that these individual functions dont each run once to add 1 character to randomChars and THEN the for loop below adds charcters = to pwLength
-    options.pwLength--
   }
   if (options.numbersConfirm) {
     allCharacters = allCharacters.concat(numbers);
     //will push a random amount of numbers using math.random
-    randomChars.push(randomNum(numbers));
-    options.pwLength--
   }
   if (options.lowerCaseConfirm) {
     allCharacters = allCharacters.concat(lowerCaseLetters);
     //will push a random amount of lower case letters using math.random
-    randomChars.push(randomNum(lowerCaseLetters));
-    options.pwLength--
   }
   if (options.upperCaseConfirm) {
     allCharacters = allCharacters.concat(upperCaseLetters);
     //will push a random amount of upper case letters using math.random
-    randomChars.push(randomNum(upperCaseLetters));
-    options.pwLength--
   }
-
   //for loop to add all types of characters into one array
-
   for (let i = 1; i <= options.pwLength; i++) {
     randomChars.push(randomNum(allCharacters));
   }
